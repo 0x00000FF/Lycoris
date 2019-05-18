47719fc095e4507676f6b2e17c93f15851f2770e
P.Knowledge <knowledge@patche.me>
2019-05-18 11:28:41 +0900

    .NET Prototyping

---
+++ Sources/LycorisServer.Core/LobbyServer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LycorisServer.Core
+{
+    class LobbyServer
+    {
+        public int ExitResult { get; set;}
+
+        public static LobbyServer Begin(string[] args)
+        {   
+            var config = LobbyServerConfig.Parse(args);
+            return new LobbyServer(config);
+        }
+
+        protected LobbyServer(LobbyServerConfig args)
+        {
+
+        }
+
+        private void AcceptCallback(IAsyncResult iar)
+        {
+
+        }
+    }
+}
---
+++ Sources/LycorisServer.Core/LobbyServerConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LycorisServer.Core
+{
+    static class LobbyServerConfig
+    {
+        public static ServerModels.ConfigModel Parse(string[] args)
+        {
+            return new ServerModels.ConfigModel()
+            {
+
+            };
+        }
+    }
+}
